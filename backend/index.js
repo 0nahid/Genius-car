5d038f463d0cd25854a6d10c41c8a1b2d6e2d573
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-03-01 20:27:48 +0600

    added dotenv

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+require("dotenv").config();
+console.log(process.env); // remove this after you've confirmed it working
+
 app.get("/", (req, res) =>
   res.send({ status: `Server is running on port ${port}` })
 );
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log({ status: `Server is running on port ${port}` })
+);
