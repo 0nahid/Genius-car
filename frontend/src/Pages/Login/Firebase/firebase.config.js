d867d7d566e73173e9c9fd0980802f49935f8631
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-03-01 17:13:12 +0600

    project started

--- frontend/src/Pages/Login/Firebase/firebase.config.js
+++ frontend/src/Pages/Login/Firebase/firebase.config.js
@@ -1,11 +1,19 @@
-console.log(process.env);
+// console.log(process.env);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCp3h0g50Xiv0avivp62LoBTIAXkaqs8Oc",
+  authDomain: "auth-template-firebase.firebaseapp.com",
+  projectId: "auth-template-firebase",
+  storageBucket: "auth-template-firebase.appspot.com",
+  messagingSenderId: "151434870168",
+  appId: "1:151434870168:web:b718d93d5b27f17ded7fed",
+  measurementId: "G-7SWV1Z2V0H",
 };
-
 export default firebaseConfig;
