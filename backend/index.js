722f11571fd0015996730119b62838d07b787f44
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-03-01 23:34:52 +0600

    user sent to db

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 require("dotenv").config();
+//middleware
+app.use(cors());
+app.use(express.json());
 
 // Database
 
@@ -14,10 +18,14 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 client.connect((err) => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("service").collection("services");
+  //   post api on server
+  app.post("/services", (req, res) => {
+    collection.insertOne(req.body, (err, result) => res.json(result));
+  });
   // perform actions on the collection object
   err ? console.log(err) : console.log("Connected to Database");
-  client.close();
+  //   client.close();
 });
 
 app.get("/", (req, res) =>
--- frontend/src/Pages/AddService/AddService.js
+++ frontend/src/Pages/AddService/AddService.js
@@ -1,7 +1,43 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import "./AddService.css";
+
 export default function AddService() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    axios
+      .post("http://localhost:5000/services", data)
+      .then((res) => {
+        if (res.data.insertedId) {
+          // shows sweet alert
+          Swal.fire("Good job!", "User added to database!", "success");
+          data.target.reset();
+        } else {
+          Swal.fire("Oops!", "Something went wrong!", "error");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
   return (
-    <>
-      <h1>This is add service</h1>
-    </>
+    <div className="add-user">
+      <h1>Please add a service</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="Name"
+          {...register("firstName", { required: true, maxLength: 20 })}
+        />
+        <textarea placeholder="Description" {...register("description")} />
+        <input placeholder="Price" type="number" {...register("price")} />
+        <input placeholder="Enter Image url" {...register("img")} />
+        <input type="submit" />
+      </form>
+    </div>
   );
 }
